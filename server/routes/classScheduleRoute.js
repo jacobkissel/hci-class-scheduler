3edfdf2fdd161ccc98e198a383774b8e1051edf1
unknown <jacobkissel@ufl.edu>
2018-10-27 19:28:58 -0400

    departments and programLevels endpoints

--- server/routes/classScheduleRoute.js
+++ server/routes/classScheduleRoute.js
@@ -3,5 +3,7 @@ var classSchedules = require('../controllers/classScheduleController.js'),
     router = express.Router();
 
 router.route("").post(classSchedules.search);
+router.route("/programLevels").get(classSchedules.programLevels);
+router.route("/departments").get(classSchedules.departments);
 
 module.exports = router;
